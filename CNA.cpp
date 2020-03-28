492966e8570bf0608cb02896769dc9d6be52f984
13ochenyi97 <55529942+13ochenyi97@users.noreply.github.com>
2020-03-28 17:14:58 +0800

    Add files via upload
    updated cna files

--- CNA.cpp
+++ CNA.cpp
@@ -1,57 +1,25 @@
-/*#pragma once
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <curl/curl.h>
 #include <algorithm>
 #include <string>
+#include <cpr/cpr.h>
+#include <Windows.h>
 #include "rapidjson/document.h"
 #include "CNA.h"
-
-//For writting output into string
-static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
 using namespace std;
 using namespace rapidjson;
 
-void Article::createCSV(string fileName, vector<Article> articles) {
-    string title, source, content, date, url, category;
-    ofstream newsCSV;
-    newsCSV.open(fileName + ".csv");
-    //Added to support encoding of UTF-8 characters to output to csv file
-    const char* bom = "\xef\xbb\xbf";
-    newsCSV << bom;
-    newsCSV << "Title, Source, Date, Content, URL, Category" << endl;
-    for (int i = 0; i < articles.size(); i++) {
-        title = articles[i].getTitle();
-        for (int j = 0; j < title.length(); ++j) {
-            // Prevent separation of columns by replacing commas with semicolon
-            if (title[j] == ',') {
-                title[j] = ';';
-            }
-        }
-        title.erase(std::remove(title.begin(), title.end(), ','), title.end());
-        source = articles[i].getSource();
-        date = articles[i].getDate();
-        content = articles[i].getContent();
-        for (int k = 0; k < content.length(); ++k) {
-            //Prevent separation of columns by replacing commas with semicommas
-            if (content[k] == ',') {
-                content[k] = ';';
-            }
-        }
-        url = articles[i].getURL();
-        category = articles[i].getCategory();
-        newsCSV << title + "," << source + "," << date + "," << content + "," << url + "," << category << "\n";
-    }
-    newsCSV.close();
+// For writing output into string
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
-
 string ReplaceStuff(string str, const string& from, const string& to) {
     size_t start_pos = 0;
-    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+    while ((start_pos = str.find(from, start_pos)) != string::npos) {
         str.replace(start_pos, from.length(), to);
         start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
     }
@@ -64,13 +32,86 @@ string queryString(string s) {
     s = ReplaceStuff(s, "\"", "%22");
     return s;
 }
+// create normal csv
+void Article::createCSV(string fileName, vector<Article> articles) {
+    string title, source, content, date, url, category;
+    ofstream cnaCSV;
+    cnaCSV.open(fileName + ".csv");
+    // added to support encoding of UTF-8 characters to output to csv file
+    const char* bom = "\xef\xbb\xbf";
+    cnaCSV << bom;
+    cnaCSV << "Title, Source, Date, Content, URL, Category" << endl;
+    for (int i = 0; i < articles.size(); i++) {
+        title = articles[i].getTitle();
+        for (int j = 0; j < title.length(); ++j) {
+            // prevent separation of columns by replacing commas with semicolons
+            if (title[j] == ',') {
+                title[j] = ';';
+            }
+        }
+        title.erase(remove(title.begin(), title.end(), ','), title.end());
+        source = articles[i].getSource();
+        date = articles[i].getDate();
+        content = articles[i].getContent();
+        for (int k = 0; k < content.length(); ++k) {
+            // prevent separation of columns by replacing commas with semicolons
+            if (content[k] == ',') {
+                content[k] = ';';
+            }
+        }
+        url = articles[i].getURL();
+        category = articles[i].getCategory();
+        cnaCSV << title + "," << source + "," << date + "," << content + "," << url + "," << category << "\n";
+    }
+    cnaCSV.close();
+}
+// create analyzed csv
+void Article::createAnalyzedCSV(string fileName, vector<Article> articles, vector<string> classificationList, vector<string> relevanceList) {
+    string title, source, content, date, url, category, classification, relevance;
+    ofstream newsCSV;
+    newsCSV.open(fileName + "_Analyzed.csv");
+    // added to support encoding of UTF-8 characters to output to csv file with Byte-Order Mark (BOM)
+    // source: https://stackoverflow.com/questions/21272006/how-to-save-a-csv-file-as-utf-8
+    const char* bom = "\xef\xbb\xbf";
+    newsCSV << bom;
+    newsCSV << "Title, Source, Date, Content, URL, Category, Classification, Relevance" << endl;
+    for (int i = 0; i < articles.size(); i++) {
+        title = articles[i].getTitle();
+        for (int j = 0; j < title.length(); ++j) {
+            // prevent separation of columns by replacing commas with semicolons
+            if (title[j] == ',') {
+                title[j] = ';';
+            }
+        }
+        source = articles[i].getSource();
+        date = articles[i].getDate();
+        content = articles[i].getContent();
+        for (int k = 0; k < content.length(); ++k) {
+            // prevent separation of columns by replacing commas with semicolons
+            if (content[k] == ',') {
+                content[k] = ';';
+            }
+        }
+        url = articles[i].getURL();
+        category = articles[i].getCategory();
+        classification = classificationList[i];
+        for (int l = 0; l < classification.length(); ++l) {
+            // prevent separation of columns by replacing commas with semicommas
+            if (classification[l] == ',') {
+                classification[l] = ';';
+            }
+        }
+        relevance = relevanceList[i];
+        newsCSV << title + "," << source + "," << date + "," << content + "," << url + "," << category + "," << classification + "," << relevance << "\n";
+    }
+    newsCSV.close();
+}
 
 vector<string> Article::analyzeClassification(vector<Article> article) {
-    CURL* curl;
-    CURLcode res;
     const string MEANINGCLOUDCLASSURL = "https://api.meaningcloud.com/class-1.1";
     const string APIKEY = "?key=d1d248b93272995b72eb5b36b6035f66";
-    // string result, text;
+    CURL* curl;
+    CURLcode res;
     Document doc;
     CNA cna;
     string classification, result, relevance, text;
@@ -79,20 +120,14 @@ vector<string> Article::analyzeClassification(vector<Article> article) {
     vector<string> allRelevance;
     for (int i = 0; i < article.size(); i++) {
         curl = curl_easy_init();
-        // text = articleTitle;
         text = article[i].getContent();
         text = queryString(text);
-        // cout << endl;
-        // cout << "Query format: " << text << endl;
         string apiURL = "";
         apiURL.append(MEANINGCLOUDCLASSURL);
         apiURL.append(APIKEY);
         apiURL.append("&of=json");
         apiURL.append("&txt=" + text);
         apiURL.append("&model=IPTC_en");
-        //converting string to const char *
-        // cout << endl;
-        // cout << "api url -> " << apiURL.c_str() << endl;
         if (curl) {
             curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
             curl_easy_setopt(curl, CURLOPT_URL, apiURL.c_str());
@@ -109,9 +144,7 @@ vector<string> Article::analyzeClassification(vector<Article> article) {
             std::cout << "Text classification failed for " << text << endl;
         }
         curl_easy_cleanup(curl);
-        std::cout << endl;
-        // cout << "Text Classification Result: " << result << endl;
-        std::cout << "Result: " << result << endl;
+        std::cout << "\nResult: " << result << endl;
         allClassificationsJSON.push_back(result);
         result = "";
     }
@@ -144,57 +177,22 @@ vector<string> Article::analyzeClassification(vector<Article> article) {
     cna.createAnalyzedCSV("CNA", article, allClassifications, allRelevance);
     return allClassifications;
 }
-void Article::createAnalyzedCSV(string fileName, vector<Article> articles, vector<string> classificationList, vector<string> relevanceList) {
-    string title, source, content, date, url, category, classification, relevance;
-    std::ofstream newsCSV;
-    newsCSV.open(fileName + "_Analyzed.csv");
-    //Added to support encoding of UTF-8 characters to output to csv file with Byte-Order Mark (BOM)
-    //Source: https://stackoverflow.com/questions/21272006/how-to-save-a-csv-file-as-utf-8
-    const char* bom = "\xef\xbb\xbf";
-    newsCSV << bom;
-    newsCSV << "Title, Source, Date, Content, URL, Category, Classification, Relevance" << endl;
-    for (int i = 0; i < articles.size(); i++) {
-        title = articles[i].getTitle();
-        for (int j = 0; j < title.length(); ++j) {
-            //Prevent separation of columns by replacing commas with semicommas
-            if (title[j] == ',') {
-                title[j] = ';';
-            }
-        }
-        source = articles[i].getSource();
-        date = articles[i].getDate();
-        content = articles[i].getContent();
-        for (int k = 0; k < content.length(); ++k) {
-            //Prevent separation of columns by replacing commas with semicommas
-            if (content[k] == ',') {
-                content[k] = ';';
-            }
-        }
-        url = articles[i].getURL();
-        category = articles[i].getCategory();
-        classification = classificationList[i];
-        for (int l = 0; l < classification.length(); ++l) {
-            //Prevent separation of columns by replacing commas with semicommas
-            if (classification[l] == ',') {
-                classification[l] = ';';
-            }
-        }
-        relevance = relevanceList[i];
-        newsCSV << title + "," << source + "," << date + "," << content + "," << url + "," << category + "," << classification + "," << relevance << "\n";
-    }
-    newsCSV.close();
-}
+
 vector<int> Article::sentimentAnalysis(vector<Article> article) {
+    const string MEANINGCLOUDSENTIMENTURL = "https://api.meaningcloud.com/sentiment-2.1";
+    const string APIKEY = "?key=09a2709de3d1e0d7a59e3ef3b8fe0fce";
     CURL* curl;
     CURLcode res;
     Document doc;
-    int score;
-    string confidence, subjectivity, score_tag, result, text;
-    curl = curl_easy_init();
-    const string MEANINGCLOUDSENTIMENTURL = "https://api.meaningcloud.com/sentiment-2.1";
-    const string APIKEY = "?key=09a2709de3d1e0d7a59e3ef3b8fe0fce";
-    vector<string> allSentimentJSON, allConfidence, allSubjectivity, allScoreTag;
-    vector<int> subjectivityQuantity;
+    string result, text, score_tag, subjectivity, confidence;
+    vector<string> allSentimentJSON;
+    vector<int> sentimentArray;
+    sentimentArray.push_back(0);
+    sentimentArray.push_back(0);
+    sentimentArray.push_back(0);
+    sentimentArray.push_back(0);
+    sentimentArray.push_back(0);
+    sentimentArray.push_back(0);
     for (int i = 0; i < article.size(); i++) {
         curl = curl_easy_init();
         text = article[i].getContent();
@@ -220,20 +218,30 @@ vector<int> Article::sentimentAnalysis(vector<Article> article) {
             res = curl_easy_perform(curl);
         }
         else {
-            cout << "Sentimental Analysis failed for " << text << endl;
-            cout << "Result: " << result << endl;
-            allSentimentJSON.push_back(result);
-            result = "";
+            std::cout << "Sentimental Analysis failed for " << text << endl;
         }
         curl_easy_cleanup(curl);
+        std::cout << endl;
+        std::cout << "Result: " << result << endl;
+        allSentimentJSON.push_back(result);
+        result = "";
     }
-    //Do JSON crawling on retrieval of classifications result
+    std::cout << "\n\nSentimental results: ";
+    //Do JSON crawling on retrieval of sentimental result
     for (int j = 0; j < allSentimentJSON.size(); j++) {
         doc.Parse(allSentimentJSON[j].c_str());
         string status = doc["status"].GetObjectW()["code"].GetString();
         if (status == "0") {
-
+            if (doc.HasMember("score_tag")) {
+                std::cout << "\nScore_Tag Test: " << doc["score_tag"].GetString() << endl;
+            }
+            if (doc.HasMember("confidence")) {
+                std::cout << "Confidence Test: " << doc["confidence"].GetString() << endl;
+            }
+            if (doc.HasMember("subjectivity")) {
+                std::cout << "Subjectivity Test: " << doc["subjectivity"].GetString() << endl;
+            }
         }
     }
-    return subjectivityQuantity;
-}*/
+    return sentimentArray;
+}
