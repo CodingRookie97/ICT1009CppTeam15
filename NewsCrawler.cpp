a5617a2fbe9403b519d89be9b43506c5a50b5277
Jerone Poh <pohjerone97@gmail.com>
2020-03-28 23:01:17 +0800

    Managed to sort the classifications of news articles in top 5. (Descending) order.

--- NewsCrawler.cpp
+++ NewsCrawler.cpp
@@ -1,28 +1,37 @@
 #pragma once
 #include <iostream>
+#include <algorithm>
 #include <cpr/cpr.h>
 #include <windows.h>
 #include "rapidjson/document.h"
 #include "Article.h"
 #include "TOC.h"
+#include "CNA.h"
 
 using namespace std;
 using namespace rapidjson;
 
+//Source: https://www.educative.io/edpresso/how-to-sort-a-map-by-value-in-cpp
+//This is the utility comparator function to compare the occurences (value) in descending order and pass to the sort() module
+bool sortByValDesc(const pair<string, int>& a, const pair<string, int>& b) {
+	return (a.second > b.second);
+}
 int main(void) {
 	//Initializes the constants
 	const int PAGESIZE = 100;
+	const string CNAURL = "channelnewsasia.com";
 	const string TOCURL = "theonlinecitizen.com";
 	const string APIKEY = "ad80aa4ebd9c4f849cd8ee1636eb84da";
 	const string THEONLINECITIZEN = "TOC";
-	//Filtered dictionary to determine whether the news articles belongs to Singapore, Business, or else World 
+	//Filtered dictionary to determine whether the news articles belongs to Singapore, Business, or else World (For TOC)
 	string singaporeKeyWords[] = { "Singapore", "SG", "Singaporean", "multi-ministry taskforce", "Multi-Ministry Taskforce", "Baey Yam Keng", "Khaw Boon Wan", "Lee Hsien Loong", "Ong Ye Kung", "Tan Cheng Bock", "Teo Chee Hean", "People's Voice", "Reform Party", "Workers' Party", "Temasek Holdings", "HDB", "LTA", "MOE", "MOH", "NEA", "NMP", "NSP", "PAP", "SPH", "WP", "SBS Transit", "SMRT", "FairPrice", "Geylang", "Holland-Bukit Timah", "Jurong", "Kranji", "MacPherson", "Potong Pasir" };
 	string businessKeyWords[] = { "Budget", "cents", "Economic Development Board", "economy", "economic", "finance", "Finance", "income", "recession", "salary", "stock", "tax", "DBS", "EDB", "GDP", "GST", "MAS", "SGX", "STI", "$" };
 	Document doc;
 	bool canCategorise = false;
 	vector<Article> tocArticles;
-	//Creates a new child article object called TOC (The Online Citizen)
+	//Creates new child article objects
 	TOC toc;
+	CNA cna;
 	//Initialize the query URL to use for News API to return all news articles belonging to The Online Citizen
 	string getNewsArticlesURL = "http://newsapi.org/v2/everything?domains=";
 	getNewsArticlesURL.append(TOCURL);
@@ -40,7 +49,7 @@ int main(void) {
 	cout << "News Articles retrieved from " << TOCURL << endl;
 	//Deencapsulates the JSON layer to retrieve the values from specified attributes (keys)
 	//Source: https://rapidjson.org/md_doc_tutorial.html
-	for (SizeType i = 0; i < 30; i++) {
+	for (SizeType i = 0; i < 20; i++) {
 		string source;
 		string title;
 		string url;
@@ -189,15 +198,47 @@ int main(void) {
 	}
 	cout << "Meaningcloud Results" << endl;
 	//Returns the list of classifications
-	vector<string> getClassifications = toc.analyzeClassification("TOC", tocArticles);
+	vector<string> getClassifications = toc.analyzeClassification(THEONLINECITIZEN, tocArticles);
+	map<string, int> cQuantity;
 	cout << "Get Classification:" << endl;
+	//Iterate the classifications retrieved by the MeaningCloud API
+	//Source: https://stackoverflow.com/questions/34292384/counting-occurrences-of-integers-in-map-c
 	for (int i = 0; i < getClassifications.size(); i++) {
 		cout << getClassifications[i] << endl;
+		//If the classification is NOT empty, perform the adding of quantity else ignore it
+		if (getClassifications[i] != "") {
+			//If the key is not present in the map, add the new key (classification) and its value (occurence)
+			if (cQuantity.find(getClassifications[i]) == cQuantity.end()) {
+				cQuantity.insert(std::pair<string, int>(getClassifications[i], 1));//single count of current number
+			}
+			else {
+				//If the key is not present in the map, simply update its value (occurence) by incrementing
+				cQuantity[getClassifications[i]]++;
+			}
+		}
 	}
-	//Returns the list of sentiment results that will be populated to pue chart
+	cout << "All Classifications: " << endl;
+	//Iterate every entry of the map
+	for (std::map<string, int>::iterator it = cQuantity.begin(); it != cQuantity.end(); it++) {
+		cout << it->first << ": " << it->second << " time(s)" << endl;
+	}
+	//Source: https://www.educative.io/edpresso/how-to-sort-a-map-by-value-in-cpp
+	//Create a empty vector of pairs
+	vector<pair<string, int>> cQuantityVec;
+	//Copy key-value pairs from the map to the newly created vector
+	for (std::map<string, int>::iterator it = cQuantity.begin(); it != cQuantity.end(); it++) {
+		cQuantityVec.push_back(make_pair(it->first, it->second));
+	}
+	//Sort the vector by decreasing order of its pair's second value, this will be populated into the bar chart to display the Top 5 News Classifications
+	sort(cQuantityVec.begin(), cQuantityVec.end(), sortByValDesc);
+	cout << "All Classifications (Sorted) In Descending Order: " << endl;
+	for (int j = 0; j < cQuantityVec.size(); j++) {
+		cout << cQuantityVec[j].first << ": " << cQuantityVec[j].second << " time(s)" << endl;
+	}
+	//Returns the list of sentiment results that will be populated to pie chart
 	vector<int> getSentiments = toc.sentimentAnalysis(tocArticles);
 	cout << "Get Sentiments:" << endl;
-	for (int i = 0; i < getSentiments.size(); i++) {
-		cout << getSentiments[i] << endl;
+	for (int k = 0; k < getSentiments.size(); k++) {
+		cout << getSentiments[k] << endl;
 	}
 }
